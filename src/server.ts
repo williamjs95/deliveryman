14e427f2bfaaf466f0d2e61fd67b5e7e837b6107
williamjs95 <williamj.silva@live.com>
2024-07-25 20:51:30 -0300

    chore: Add scripts for development and Prisma commands

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { request } from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' })
+})
+
+app.listen(3000, () => console.log('Server is running on port 3000'))
