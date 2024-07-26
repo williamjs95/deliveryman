78fd403f7636aedb432264b3eece8a47cc1d6f44
williamjs95 <williamj.silva@live.com>
2024-07-26 00:46:34 -0300

    chore: Update package.json scripts and add routes module

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,34 @@
+import { prisma } from '../../../../database/prismaClient';
+import { hash } from 'bcrypt';
+
+interface ICreateClient {
+  username: string
+  password: string
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreateClient) {
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: 'insensitive',
+        }
+      }
+    })
+
+    if (clientExists) {
+      throw new Error('Client already exists');
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashedPassword
+      }
+    })
+
+    return client;
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express, { request } from 'express'
+import { routes } from './routes'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(express.json())
+
+app.use(routes)
+
+
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
