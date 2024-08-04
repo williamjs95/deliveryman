50b6a27c1907f13254fd219130e5e4875e87825e
williamjs95 <williamj.silva@live.com>
2024-08-04 18:01:13 -0300

    feat: Add route to find all available deliveries

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { CreateDeliverymanController } from './modules/deliveryman/useCases/crea
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController'
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController'
 import { ensureAuthenticateClient } from './middlewares/ensureAuthenticateClient'
+import { FindAllAvailableController } from './modules/deliveries/useCases/findAllAvailable/FindAllAvailableController'
 
 
 const routes = Router();
@@ -13,8 +14,8 @@ const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
-
 const deliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
 
 routes.post('/client/authenticate', authenticateClientController.handle)
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
@@ -23,5 +24,6 @@ routes.post('/client/', createClientController.handle)
 routes.post('/deliveryman/', createDeliverymanController.handle)
 
 routes.post('/delivery', ensureAuthenticateClient, deliveryController.handle)
+routes.get('/delivery/available', findAllAvailableController.handle) 
   
 export { routes };
