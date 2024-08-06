4af80eec53fe2668cc282ae555766f2a86d354f9
williamjs95 <williamj.silva@live.com>
2024-08-05 22:13:29 -0300

    feat: Add authentication middleware for deliveryman route

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { AuthenticateDeliverymanController } from './modules/account/authenticat
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController'
 import { ensureAuthenticateClient } from './middlewares/ensureAuthenticateClient'
 import { FindAllAvailableController } from './modules/deliveries/useCases/findAllAvailable/FindAllAvailableController'
+import { ensureAuthenticateDeliveryman } from './middlewares/ensureAuthenticateDeliveryman'
 
 
 const routes = Router();
@@ -24,6 +25,6 @@ routes.post('/client/', createClientController.handle)
 routes.post('/deliveryman/', createDeliverymanController.handle)
 
 routes.post('/delivery', ensureAuthenticateClient, deliveryController.handle)
-routes.get('/delivery/available', findAllAvailableController.handle) 
+routes.get('/delivery/available', ensureAuthenticateDeliveryman, findAllAvailableController.handle) 
   
 export { routes };
